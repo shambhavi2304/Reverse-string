1b073251579c5f6c972559820565e25c6d9dce77
shambhavi2304 <23103004@mail.jiit.ac.in>
2025-08-04 20:27:02 +0530

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove trailing newline if present
+    str[strcspn(str, "\n")] = 0;
+
+    int len = strlen(str);
+    printf("Reversed string: ");
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
